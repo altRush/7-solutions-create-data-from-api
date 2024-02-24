248850f55e71de3178afa47d20284bd3d133c846
Waraht Punyarote <rushing.wp@gmail.com>
2024-02-24 18:06:29 +0700

    Revert "build: Change port to 3001"
    This reverts commit cc7920c07afa03851256d5b969caabb3f5d0204e.

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Express, Request, Response } from 'express';
 
 const app: Express = express();
 
-const port: number = 3001;
+const port: number = 3000;
 
 app.get('/', (req: Request, res: Response) => {
 	res.json({
