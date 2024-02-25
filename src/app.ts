693225af2394e808748c1688bde1f6a1695fe18e
Waraht Punyarote <rushing.wp@gmail.com>
2024-02-25 21:01:14 +0700

    feat: Handle unhappy cases in main app

--- src/app.ts
+++ src/app.ts
@@ -9,17 +9,40 @@ const port: number = 3000;
 app.get('/', async (req: Request, res: Response) => {
 	const userLimit = req.query.limit;
 
-	const {
-		data: { users }
-	} = await axios.get(
-		`https://dummyjson.com/users${userLimit ? `?limit=${userLimit}` : ''}`
-	);
+	let users;
+
+	try {
+		({
+			data: { users }
+		} = await axios.get(
+			`https://dummyjson.com/users${userLimit ? `?limit=${userLimit}` : ''}`
+		));
+	} catch (e) {
+		console.error('Unexpected error: ', e);
+		res.status(500).json({
+			message: 'Internal server error'
+		});
+	}
+
+	if (!users?.length) {
+		res.status(400).json({ message: 'Invalid supplied users data' });
+	}
 
 	const groupedByDepartmentData = groupBy(users, 'company', 'department');
 
+	if (!groupedByDepartmentData) {
+		res.status(400).json({ message: 'Insufficient grouped department data' });
+	}
+
 	const formedResponse = formResponse(groupedByDepartmentData);
 
-	res.json(formedResponse);
+	if (!formedResponse) {
+		res.status(400).json({
+			message: 'Insufficient formed response by grouped department data'
+		});
+	}
+
+	res.status(200).json(formedResponse);
 });
 
 app.listen(port, () => console.log(`Application is running on port ${port}`));
