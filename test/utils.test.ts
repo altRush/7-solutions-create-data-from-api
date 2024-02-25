25bcad9ab5d429d2a0fcd5f3c9da92063995a834
Waraht Punyarote <rushing.wp@gmail.com>
2024-02-25 13:41:45 +0700

    test: Draft test suites

---
+++ test/utils.test.ts
@@ -0,0 +1,38 @@
+import {
+	groupBy,
+	extractCountOfDataGroup,
+	findMinMax,
+	transformToNameAndPostalCode,
+	formResponseRecordForDepartment
+} from '../src/utils';
+
+describe('All utils', () => {
+	test('groupBy', () => {
+		test('nested', () => {});
+		test('not nested', () => {});
+		test('empty', () => {});
+	});
+
+	test('extractCountOfDataGroup', () => {
+		test('successful', () => {});
+		test('early exit', () => {});
+	});
+
+	test('findMinMax', () => {
+		test('successful', () => {});
+		test('early exit', () => {});
+	});
+
+	test('transformToNameAndPostalCode', () => {
+		test('successful', () => {});
+		test('early exit', () => {});
+	});
+
+	test('formResponseRecordForDepartment', () => {
+		test('successful', () => {});
+	});
+
+	test('formResponse', () => {
+		test('successful', () => {});
+	});
+});
